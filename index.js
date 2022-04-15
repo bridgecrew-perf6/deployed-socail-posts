a92daf6ccaeb384bbf2b4932721c411903b6d092
jayasimha <jayasimhareddy27@gmai.com>
2022-04-15 14:58:46 +0530

    make it better

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createRequire } from "module";
+
 import Express  from "express";
 import cors from 'cors';
 import mongoose from "mongoose";
@@ -6,17 +8,38 @@ import dotenv from 'dotenv';
 
 import postrouter from './routes/postroutes.js';
 import userrouter from './routes/userroutes.js';
+import { fileURLToPath } from 'url';
+const require = createRequire(import.meta.url);
 
 const app=Express();
 app.use(cors());
-app.use(Express.static("public"));
+const path = require("path");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 dotenv.config();
 
-app.use('/posts',postrouter);
-app.use('/user',userrouter)
-app.get('/',(req,res)=>{res.send('APP IS RUNNING')});
+
+app.use('s/posts',postrouter  );
+app.use('s/user',userrouter)
+
+if ( process.env.NODE_ENV == "production"){
+    app.use(Express.static(path.join(__dirname,"client/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));  
+    })
+}else{
+    app.get("/", (req, res) => {
+        res.send("API RUNNING");
+    })
+}
+
+
+
 const CONNECTION_URL =process.env.CONNECTION_URL;
 const PORT = process.env.PORT|| 5000;
 
